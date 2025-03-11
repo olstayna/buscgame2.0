667507b66f0e77310e13256788e56d43b5c03e86
Tayná Oliveira <67444028+olstayna@users.noreply.github.com>
2025-03-10 21:00:41 -0300

    fixing variables to vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: true,
     apiUrl: 'https://api.isthereanydeal.com',
-    apiKey: process.env['API_KEY']
+    apiKey: ''
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: false,
     apiUrl: 'https://api.isthereanydeal.com',
-    apiKey: 'development_key'
+    apiKey: ''
 }; 
